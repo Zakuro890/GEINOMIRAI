c50775c35b2836f5171117973c2cda4c29bd7ed4
okawauchi kazuya <102887065+Zakuro890@users.noreply.github.com>
2024-05-02 19:06:54 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,108 @@
+//ハンバーガーメニューをクリックした際の処理
+$(function() {
+	$('#menubar_hdr').click(function() {
+		$(this).toggleClass('ham');
+
+			if($(this).hasClass('ham')) {
+				$('#menubar').addClass('d-b');
+			} else {
+				$('#menubar').removeClass('d-b');
+			}
+
+	});
+});
+
+// 同一ページへのリンクの場合に開閉メニューを閉じる処理
+$(function() {
+	$('#menubar a[href^="#"]').click(function() {
+		$('#menubar').removeClass('d-b');
+		$('#menubar_hdr').removeClass('ham');
+	});
+});
+
+
+// 汎用開閉処理
+$(function() {
+	$('.openclose').next().hide();
+	$('.openclose').click(function() {
+		$(this).next().slideToggle();
+		$('.openclose').not(this).next().slideUp();
+	});
+});
+
+//桜エフェクト
+particlesJS("particles-js", {
+  "particles":{
+    "number":{
+      "value":30,//この数値を変更すると桜の数が増減できる
+      "density":{
+        "enable":true,
+        "value_area":1121.6780303333778
+      }
+    },
+    "color":{
+      "value":"#fff"
+    },
+    "shape":{
+      "type":"image",//形状は画像を指定
+      "stroke":{
+        "width":0,
+      },
+      "image":{
+        "src":"http://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/move02/5-6/img/flower.png",//画像を指定
+        "width":120,
+        "height":120
+      }
+    },
+    "opacity":{
+      "value":0.06409588744762158,
+      "random":true,
+      "anim":{
+        "enable":false,
+        "speed":1,
+        "opacity_min":0.1,
+        "sync":false
+      }
+    },
+    "size":{
+      "value":8.011985930952697,
+      "random":true,//サイズをランダムに
+      "anim":{
+        "enable":false,
+        "speed":4,
+        "size_min":0.1,
+        "sync":false
+      }
+    },
+    "line_linked":{
+      "enable":false,
+    },
+    "move":{
+      "enable":true,
+      "speed":7,//この数値を小さくするとゆっくりな動きになる
+      "direction":"bottom-right",//右下に向かって落ちる
+      "random":false,//動きはランダムにしない
+      "straight":false,//動きをとどめない
+      "out_mode":"out",//画面の外に出るように描写
+      "bounce":false,//跳ね返りなし
+      "attract":{
+        "enable":false,
+        "rotateX":281.9177489524316,
+        "rotateY":127.670995809726
+      }
+    }
+  },
+  "interactivity":{
+    "detect_on":"canvas",
+    "events":{
+      "onhover":{
+        "enable":false,
+      },
+      "onclick":{
+        "enable":false,
+      },
+      "resize":true
+    }
+  },
+  "retina_detect":false
+});
